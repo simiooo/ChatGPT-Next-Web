22c4238a3fcb65001544a95c1f51ed7faa8696bf
simooowann <simooowann@gmail.com>
2024-04-29 23:08:00 +0800

    修改参数

--- app/constant.ts
+++ app/constant.ts
@@ -10,7 +10,7 @@ export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export const DEFAULT_CORS_HOST = "https://a.nextweb.fun";
 export const DEFAULT_API_HOST = `${DEFAULT_CORS_HOST}/api/proxy`;
-export const OPENAI_BASE_URL = "https://openai-sb.com";
+export const OPENAI_BASE_URL = "https://api.openai-sb.com";
 
 export enum Path {
   Home = "/",
